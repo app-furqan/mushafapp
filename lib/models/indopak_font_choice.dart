2d59542d2236d8a250ff724a9d2d163741890570
Hussain Khan <hussainak@gmail.com>
2026-03-21 00:40:14 +1100

    Added taj and qudratullah mushaf

--- lib/models/indopak_font_choice.dart
+++ lib/models/indopak_font_choice.dart
@@ -1,8 +1,15 @@
-enum IndopakFontChoice { indopak, kfgqpcNastaleeq }
+enum IndopakFontChoice {
+  indopak,
+  kfgqpcNastaleeq,
+  digitalKhattIndoPak,
+  nastaleeq,
+}
 
 extension IndopakFontChoiceInfo on IndopakFontChoice {
   String get label => switch (this) {
     IndopakFontChoice.indopak => 'IndoPak',
     IndopakFontChoice.kfgqpcNastaleeq => 'KFGQPC Nastaleeq',
+    IndopakFontChoice.digitalKhattIndoPak => 'DigitalKhatt IndoPak',
+    IndopakFontChoice.nastaleeq => 'Nastaleeq',
   };
 }
